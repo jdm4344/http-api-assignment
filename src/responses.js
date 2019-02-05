074080842b39a00234a873169b1605feb61bc0e8
Jordan Machalek <jdm4344@rit.edu>
2019-02-05 14:57:38 -0500

    Started setting up urls

---
+++ src/responses.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+
+const respond = (request, response, content, type) => {
+    response.writeHead(200, { 'Content-Type': type });
+    response.write(content);
+    response.end();
+};
+  
+const getIndex = (request, response) => {
+    respond(request, response, index, 'text/html');
+};
+
+const jsonSuccess = () => {
+
+};
+
+const jsonFailure = () => {
+
+};
+
+const xmlSuccess = () => {
+
+};
+
+const xmlFailure = () => {
+
+};
--- src/server.js
+++ src/server.js
@@ -1,1 +1,31 @@
 const http = require("http");
+const url = require('url');
+const responseHandler = require('./responses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+    "/": responseHandler.getIndex,
+    "/badrequest": responseHandler.getIndex,
+    "/unauthorized": responseHandler.getIndex,
+    "/forbidden": responseHandler.getIndex,
+    "/internal": responseHandler.getIndex,
+    "/notImplemented": responseHandler.getIndex,
+    index: responseHandler.getIndex,
+};
+
+const onRequest = (request, response) => {
+    const parsedUrl = url.parse(request.url);
+    const acceptedTypes = request.headers.accept.split(","); // header is a string divided by commas
+
+    // Check if request is for XML, if not, send JSON
+    if(urlStruct[parsedUrl.pathname]){
+        urlStruct[parsedUrl.pathname](request, response, acceptedTypes);
+    } else{
+        urlStruct.index(request, response, acceptedTypes);
+    }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
